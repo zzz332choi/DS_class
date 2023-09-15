daae4e29420a2084a1287d7f41f7f860b0aa5bd3
zz332choi <chebonggyu@gmail.com>
2023-09-15 22:39:01 +0900

    today last

--- 2023_Kwangwoon_Univ_CE_DS_Project_1/src/MemberQueueNode.h
+++ 2023_Kwangwoon_Univ_CE_DS_Project_1/src/MemberQueueNode.h
@@ -11,6 +11,8 @@ private:
 	char* date = nullptr;
 	char condition_type;
 
+	MemberQueueNode* next = nullptr;
+
 public:
 	MemberQueueNode() {}
 	virtual ~MemberQueueNode() {
@@ -34,6 +36,9 @@ public:
 	void setting_condition_type(const char c) {
 		condition_type = c;
 	}
+	void setting_next(MemberQueueNode *node) {
+		next = node;
+	}
 
 	char* get_name() const {
 		return name;
@@ -46,5 +51,8 @@ public:
 	}
 	char get_condition_type() const {
 		return condition_type;
+	}
+	MemberQueueNode* get_next() const{
+		return next;
 	}
 };
