11984482ef2696bccabef1cdc5533f9c48382ca9
zzz332choi <123616196+zzz332choi@users.noreply.github.com>
2023-11-08 20:30:09 +0900

    practice data_structure

---
+++ Class_Definition_for_Winner_Trees.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+
+#define TreeSize 20
+
+typedef struct Element {
+// whatever data
+	int key;
+};
+
+class Winner {
+public:
+	Winner(Element*, int);
+private:
+	int* winner; // stores only k-1 internal nodes
+	int k; // size of winner
+};
+
+Winner::Winner(Element* R, int sz = TreeSize) // min winner tree
+{
+	k = sz;
+	winner = new int[k]; // Don't want to use winner[0]
+	for (int i = 1; i < k; i++) winner[i] = -1; // initial -1
+
+	int j = k;
+	for (int i = k - 1; i >= k / 2 && j != -1; i--) { // 작은게 이기는 거임
+		// Play a tournament at each leaf of the tree
+		if (R[j].key > R[j - 1].key) winner[i] = j - 1;
+		else winner[i] = j;
+		j -= 2; // 두 칸씩 건너 뛴다. 
+	}
+
+	if (j == 1) { // k가 홀수 // 부전승의 개념 이용 일단 R1만 남는데 이미 이겼다고 보고 그의 왼쪽 자노드(즉, leaf node중 k-1과 k 중 이긴 element와 대결
+		// The tree contains a node which has one child
+		// Play a tournamet at this node
+		winner[k / 2] = 1;
+		int challenger = winner[k / 2 * 2]; // leaf node중 k-1th or kth 이긴 애가 위치한 곳: k/2 th node의 왼쪽 자노드
+		if (R[challenger].key < R[1].key) winner[k / 2] = challenger; // R1을 이긴 취급했다가 왼쪽 자노드가 더 작은 애였던거임(그 자노드가 winner임)
+	}
+
+	for (int i = k / 2 - 1; i >= 1; i--) { // 하위 k/2개는 다 insert함. 이제 남은 상위에 대해서 하며 배열의 0 칸은 비워놓는다.
+		// Play a tournament at each internal node of the tree starting
+		// from the bottom adn moving towards the root
+		j = 2 * i;
+		if (R[winner[j]].key > R[winner[j + 1]].key) winner[i] = winner[j + 1]; // 오른쪽 자노드가 이긴 경우
+		else winner[i] = winner[j];												// 왼쪽 자노드가 이긴 경우
+	}
+}
---
+++ Virtual_Destructor.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+// 대충 두 소멸자 앞에 어디든 virtual을 붙이면 해결되는데 그냥 다 붙이는 걸로 합의보자
+// virtual 을 붙이지 않은 경우 3000byte의 메모리 누수가 생긴다. 
+
+class Base {
+public:
+	virtual ~Base() {
+		cout << "Base destructor!" << endl;
+	}
+};
+
+class Derived : public Base {
+public: 
+	char* largeBuffer;
+	Derived() {
+		largeBuffer = new char[3000];
+	}
+
+	~Derived() {
+		cout << "Derived dextructor!" << endl;
+		delete[] largeBuffer;
+	}
+};
+
+int main() {
+	cout << "----Derived# der1 = new Derived() ----" << endl;
+	Derived* der1 = new Derived();
+	delete der1;
+
+	cout << "\n\n---Base* der2 = new Derived() ---" << endl;
+	Base* der2 = new Derived();
+	delete der2;
+}
